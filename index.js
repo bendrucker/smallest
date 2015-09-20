090b3a2c8a3e173a226a25ee867a862095eee5b3
gdi2290 <github@gdi2290.com>
2015-09-19 23:48:37 -0700

    refactor(smallest): use Math.min
    if you didn't have the undocumented feature then this would just be
    `Math.min`. If the native `Math.min` function is slow then you can use
    this algorithm
    
    ```javascript
    function smallest () {
      var values = Array.isArray(arguments[0]) ? arguments[0] : arguments
      for (var i = 0, max; i < values.length; i++) {
        max = max < values[i] ? max : values[i]
      }
      return max
    }
    ```

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 'use strict'
-
-var toArray = require('to-array')
-
 module.exports = function smallest (values) {
-  if (!Array.isArray(values)) {
-    values = toArray(arguments)
-  }
-  return values.slice().sort(order)[0]
-}
-
-function order (a, b) {
-  return a - b
+  return Math.min.apply(Math, Array.isArray(values) ? values : arguments)
 }
